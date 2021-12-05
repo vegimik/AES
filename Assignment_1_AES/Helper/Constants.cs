b18b0ab3f5dca1e9fd61a638a30edd4f36638938
vegimik <vegimi.k@hotmail.com>
2021-12-05 17:32:11 +0100

    Constant explanation

--- Assignment_1_AES/Helper/Constants.cs
+++ Assignment_1_AES/Helper/Constants.cs
@@ -2,6 +2,7 @@
 {
     public static class Constants
     {
+        //These keys should be stored somewhere safe and not exposed, but we have decided here that educational interpretation is easier
         public static string AesKey256 { get; } = "Vjhhvhkljlckjc87b4dgjuf867vhjKL9k=";
         public static string AesKey192 { get; } = "Vjhhvhkljgjuf867vhjKL9k=";
         public static string AesKey128 { get; } = "Vjhhv867vhjKL9k=";
